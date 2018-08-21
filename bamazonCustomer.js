841a04c838eccb6ddcd4200d00ef5b31fcc8ef67
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-08-20 19:41:12 -0500

    adding working functions that are pulling data

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,49 @@
-mysql = require ("mysql");
-inquirer = require ("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
+});
 
-inquirer.prompt([
-    {
-        name: "product number",
+function findItemID(arg) {
+    let query = connection.query(`SELECT item_id FROM products WHERE ?`, [{
+        item_id: arg
+    }], (err, res) => {
+        if (err) {
+            throw err;
+        };
+        console.log(res);
+    });
+};
+
+function findQuantity(arg) {
+    let query = connection.query(`SELECT stock_quantity FROM products WHERE ?`, [{
+        stock_quantity: arg
+    }], (err, res) => {
+        if (err) {
+            throw err;
+        };
+        console.log(res);
+    });
+    connection.end();
+};
+
+inquirer.prompt([{
+        name: "productID",
         message: "What is the 'ID Number' of the product you would like to buy?"
+    },
+    {
+        name: "howMany",
+        message: "How many units would you like to buy?"
     }
-]).then(function(check){
-    if (NaN){
+]).then(function (check) {
+    if (NaN) {
         throw err
     };
-    console.log(check);
+    // console.log(check.productID);
+    findItemID(check.productID);
+    findQuantity(check.howMany);
 });
