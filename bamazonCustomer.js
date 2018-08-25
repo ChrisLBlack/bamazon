9c50bd495e05eb2eebe756e18318e0045a86374d
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-08-25 09:56:07 -0500

    adding comments to the code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,12 +7,16 @@ const connection = mysql.createConnection({
     password: "password",
     database: "bamazon_DB"
 });
-
+//how much stock the customer is asking to buy
 let custHowMany = [];
+//how much the store has on hand
 let howManyOnHand = [];
+//the item id of the item the customer would like to purchase
 let itemId = [];
+//how much money the item costs
 let howMuchMoney = [];
 
+//prompt to get values for the item id and how many units of the item the customer wants to buy
 inquirer.prompt([{
         name: "productID",
         message: "What is the 'ID Number' of the product you would like to buy?"
@@ -24,9 +28,12 @@ inquirer.prompt([{
 ]).then(function (check) {
     if (NaN) {
         throw err
-    }
-    itemId.push(parseInt(check.productID))
+    };
+    //pushes item id to the array above and casts it to an integer
+    itemId.push(parseInt(check.productID));
+    //pushes how much the customer wants to the array above and casts it to an integer
     custHowMany.push(parseInt(check.howMany));
+    //runs the function and passes through the product ID of the item the customer wants
     findItemID(check.productID);
 
 });
@@ -38,15 +45,20 @@ function findItemID(arg) {
         if (err) {
             throw err;
         };
-
+        //pushes how much the stock of the store has on hand to the above array and casts to an integer
         howManyOnHand.push(parseInt(res[0].stock_quantity));
+        //pushes how much the item costs to the above array and casts to an integer with decimal
         howMuchMoney.push(parseFloat(res[0].price));
 
+        //checks to see if the items the customer wants are in stock and if so, it totals their order for them
         if (howManyOnHand[0] >= custHowMany[0]) {
+            //pushes new total of merchandise on hand, after customers purchase to above array
             howManyOnHand.push(howManyOnHand[0] - custHowMany[0]);
+            //multiplies price by how many items the customer wants
             let total = custHowMany[0] * howMuchMoney[0];
             console.log("Order placed");
             console.log(`Your total will be: $${total}`);
+            //runs function to update the stock in the database after the customers purchase
             updateStock();
 
         } else {
@@ -57,10 +69,9 @@ function findItemID(arg) {
         };
     });
 };
-
+//sends the updated stock on hand to the database and ends the connection
 function updateStock() {
     let updateSql = connection.query(`UPDATE bamazon_DB.products SET stock_quantity = ${howManyOnHand[1]} WHERE (item_id = ${itemId[0]});`, (err, res) => {
-        // console.log(res);
         if (err) {
             throw err;
         }else{
